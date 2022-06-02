ab816bfa559b17d10790e565b1cfbd92c83fdcc3
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-02 21:41:21 +0300

    uodate admin and foodapp url

--- customer/admin.py
+++ customer/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import MenuItem, Category, OrderModel
 # Register your models here.
 
-admine.site.register(MenuItem)
+admin.site.register(MenuItem)
 admin.site.register(Category)
 admin.site.register(OrderModel)
--- foodapp/urls.py
+++ foodapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from customer.views import Index About, Order
+from customer.views import Index, About, Order
 
 urlpatterns = [
     path('admin/', admin.site.urls),
